437e027378b2a2a0d759fa4d4ddc2a919428078b
Isaiah Wilson <isaiahrwilson@gmail.com>
2023-06-22 14:18:18 -0500

    Syntax Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          
+
+            /*--------------------------------------*/ //another way to comment
+
+            // this is a comment
+
+            var answer2 = 4;
+            var response2 = "";
+            
+                response2= (answer2 < 9) ?  ($"{answer2} is less than nine") : ($" {answer2}greater than or equal to nine");
+
+
+
+
+
+                
+
+
+            Console.WriteLine(response2);
+
+
+
+
+
+
+            
+
+
+
+
+           
+
         }
+
     }
 }
